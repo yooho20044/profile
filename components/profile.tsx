fef876fedc1ec793fbeddd2d182b9508c020e6a6
homeCom <rhj1303@naver.com>
2025-02-24 19:30:01 +0900

    [2025.02.24] edit main effect

--- components/profile.tsx
+++ components/profile.tsx
@@ -2,7 +2,7 @@ import { UserIcon } from '@heroicons/react/24/solid';
 
 export default function Profile(){
     return(
-        <div className="min-h-screen rounded-lg bg-neutral-200 text-black m-20 flex flex-row" data-aos="fade-right">
+        <div className="min-h-screen rounded-lg bg-white text-black m-20 flex flex-row" data-aos="fade-right">
             <div className='w-[20%] border-r-2 border-neutral-500 flex justify-center pt-10'>
                 <span className='text-4xl'>개인 사항</span>
             </div>
