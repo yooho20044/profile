2fbad684a2e7b200a32684a7543321ee423c557c
homeCom <rhj1303@naver.com>
2025-02-27 00:48:25 +0900

    [2025.02.27] top, bottom페이지 수정정

--- components/bottom.tsx
+++ components/bottom.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 
 export default function Bottom(){
 
-    const [hide, setHide] = useState(false);
+    const [hide, setHide] = useState(true);
     
             useEffect(() => {
                 const handleScroll = () => {
-                const threshold = 1000; // 문서 맨 아래로부터 얼마 남았을 때 숨길지 결정 (픽셀 단위)
+                const threshold = 1250; // 문서 맨 아래로부터 얼마 남았을 때 숨길지 결정 (픽셀 단위)
                 if (window.innerHeight + window.scrollY >= document.body.offsetHeight - threshold) {
                     setHide(false);
                 } else {
@@ -19,13 +19,17 @@ export default function Bottom(){
             }, []);
 
     return(
-        <div className="bg-black min-w-screen min-h-screen flex justify-center items-center text-white font-semibold text-8xl z-50">
+        <div className="bg-black w-screen min-h-screen flex justify-center items-center font-semibold text-8xl">
+            {/* 타이핑 효과를 적용할 컨테이너 */}
             <div 
-            className={`${hide ? "opacity-0" : "opacity-100"}`} 
-            style={{ fontSize: '100px', minWidth: '11px' }}>
-                    <span className="text-white">
-                        Thank You
+            className={`fixed ${hide ? "opacity-0" : "opacity-100"} text-[50px] min-w-[8px] md:text-[75px] md:min-w-[10px] lg:text-[100px] lg:min-w-[11px]`} 
+            >
+                {/* overflow-hidden과 border-r로 커서 효과 적용 */}
+                <div className="overflow-hidden inline-block border-r border-white animate-typing">
+                    <span className="whitespace-nowrap text-white">
+                        &lt;/Hello World&gt;
                     </span>
+                </div>
             </div>
         </div>
     )
--- components/top.tsx
+++ components/top.tsx
@@ -6,7 +6,7 @@ export default function Top(){
 
         useEffect(() => {
             const handleScroll = () => {
-            const threshold = 1250; // 문서 맨 아래로부터 얼마 남았을 때 숨길지 결정 (픽셀 단위)
+            const threshold = 900; // 문서 맨 아래로부터 얼마 남았을 때 숨길지 결정 (픽셀 단위)
             if (window.innerHeight + window.scrollY >= document.body.offsetHeight - threshold) {
                 setHide(true);
             } else {
@@ -23,12 +23,12 @@ export default function Top(){
         <div className="bg-black w-screen min-h-screen flex justify-center items-center font-semibold text-8xl">
             {/* 타이핑 효과를 적용할 컨테이너 */}
             <div 
-            className={`fixed ${hide ? "opacity-0" : "opacity-100"} text-[50px] min-w-[8px] md:text-[75px] md:min-w-[10px] lg:text-[100px] lg:min-w-[11px]`} 
+            className={`fixed text-[50px] min-w-[8px] md:text-[75px] md:min-w-[10px] lg:text-[100px] lg:min-w-[11px]`} 
             >
                 {/* overflow-hidden과 border-r로 커서 효과 적용 */}
                 <div className="overflow-hidden inline-block border-r border-white animate-typing">
                     <span className="whitespace-nowrap text-white">
-                        &lt;Hello World/&gt;
+                       {hide ? `</Hello World>` :  `<Hello World>` } 
                     </span>
                 </div>
             </div>
