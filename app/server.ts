a48edd37c44287f9c7284640004780578f1b23a7
homeCom <rhj1303@naver.com>
2025-02-28 20:00:57 +0900

    [2025.02.28] 서버 DB에서 정보 불러오는 작업 진행행

---
+++ app/server.ts
@@ -0,0 +1,15 @@
+export default async function getProject(id: number){
+    try {
+        const url = `http://jayryu.ipTime.org:2680/getProjectOne?pjId=${encodeURIComponent(id)}`;
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error('네트워크 응답이 정상적이지 않습니다.');
+        }
+        const data = await res.json();
+        return data;
+      } catch (error) {
+        console.error('프로젝트 리스트를 불러오는 중 에러 발생:', error);
+        throw error;
+      }
+
+}
--- components/employment_history.tsx
+++ components/employment_history.tsx
@@ -1,24 +1,24 @@
 "use client"
 
-import project from '@/test.json'
+import getProject from '@/app/server';
 import { useEffect, useState } from 'react';
 
 interface ProjectProps{
-    pj_id: number;
-    pj_title: string;
-    pj_company: string;
-    pj_period: string;
-    pj_man: string;
-    pj_participation?: string;
-    pj_skill: string[];
-    pj_work: string[];
-    pj_result?: string[];
-    pj_review?: string[];
+    pjId: number;
+    pjTitle: string;
+    pjCompany: string;
+    pjPeriod: string;
+    pjMan: string;
+    pjParticipation?: string;
+    skills: string[];
+    works: string[];
+    results?: string[];
+    reviews?: string[];
 }
 
-function selectId(id: number){
+async function selectId(id: number){
 
-  return project.find((proj : ProjectProps) => proj.pj_id === id);
+  return await getProject(id);
 }
 
 export default function EmploymentHistory(){
@@ -35,8 +35,11 @@ export default function EmploymentHistory(){
 
     {/** 초기화면 설정정 */}
     useEffect(() => {
-      const initialProject = selectId(1);
+      async function fetchInitialProject() {
+      const initialProject = await selectId(1);
       setSelectProject(initialProject);
+      }
+      fetchInitialProject();
     }, []);
 
     return(
@@ -50,8 +53,9 @@ export default function EmploymentHistory(){
           <div className="lg:hidden p-4">
             <select
               className="w-full p-2 border border-gray-300 rounded"
-              value={selectProject?.pj_id || ""}
-              onChange={(e) => setSelectProject(selectId(parseInt(e.target.value)))}
+              value={selectProject?.pjId || ""}
+              onChange={async(e) => {const project = await selectId(parseInt(e.target.value))
+                setSelectProject(project)}}
             >
               <optgroup label="개발 경력">
                 <option value="1">특허 빅데이터 분석 서비스 데이터 구축 사업</option>
@@ -66,30 +70,30 @@ export default function EmploymentHistory(){
             {selectProject && (
               <div className="w-full flex flex-col p-4">
                 <div className="border-b-2 border-black pb-4">
-                  <h1 className="text-center text-3xl font-bold">{selectProject.pj_title}</h1>
+                  <h1 className="text-center text-3xl font-bold">{selectProject.pjTitle}</h1>
                 </div>
                 <div className='flex flex-col pt-2 gap-2'>
-                  <p><span className='font-bold'>회사/기관명 </span>: {selectProject.pj_company}</p>
-                  <p><span className='font-bold'>프로젝트 기간 </span> : {selectProject.pj_period}</p>
-                  <p><span className='font-bold'>참여인원 </span> : {selectProject.pj_man}</p>
-                  {selectProject.pj_participation && (<p><span className='font-bold'>참여도 </span>: {selectProject.pj_participation}</p>)}
+                  <p><span className='font-bold'>회사/기관명 </span>: {selectProject.pjCompany}</p>
+                  <p><span className='font-bold'>프로젝트 기간 </span> : {selectProject.pjPeriod}</p>
+                  <p><span className='font-bold'>참여인원 </span> : {selectProject.pjMan}</p>
+                  {selectProject.pjParticipation && (<p><span className='font-bold'>참여도 </span>: {selectProject.pjParticipation}</p>)}
                   
-                  <p><span className='font-bold'>사용 기술 </span>: {selectProject.pj_skill.map(((sk,index) => <span key={index}>{sk}{index !== selectProject.pj_skill.length - 1 ? ', ' : ''}</span>))}</p>
+                  <p><span className='font-bold'>사용 기술 </span>: {selectProject.skills.map(((sk,index) => <span key={index}>{sk}{index !== selectProject.skills.length - 1 ? ', ' : ''}</span>))}</p>
                   <p><span className='font-bold'>주요 업무</span></p>
                   <div className='*:pl-3'>
-                    {selectProject.pj_work.map((work,index) =><li key={index}>{work}</li>)}
+                    {selectProject.works.map((work,index) =><li key={index}>{work}</li>)}
                   </div>
-                  {selectProject.pj_result &&(<>
+                  {selectProject.results &&(<>
                     <p><span className='font-bold'>성과 및 경험</span></p>
                     <div className='*:pl-3'>
-                      {selectProject.pj_result.map((result, index) => <li key={index}>{result}</li>)}
+                      {selectProject.results.map((result, index) => <li key={index}>{result}</li>)}
                     </div>
                   </>
                   )}
-                  {selectProject.pj_review && (<>
+                  {selectProject.reviews && (<>
                     <p><span className='font-bold text-2xl'>Review</span></p>
                     <div className='*:pl-3'>
-                      {selectProject.pj_review.map((review, index) => <li key={index}>{review}</li>)}
+                      {selectProject.reviews.map((review, index) => <li key={index}>{review}</li>)}
                     </div>
                   </>
                   )}
@@ -103,10 +107,10 @@ export default function EmploymentHistory(){
               <div className='flex flex-col  border-t-4 border-b-4 border-black w-full'>
                 <h2 className='border-b-4 h-10 border-black flex justify-center items-center'>개발 경력</h2>
                 <div className='flex-row *:h-32 *:text-md flex *:w-1/2'>
-                  <div className={`cursor-pointer ${selectProject?.pj_id === 1 ? 'bg-neutral-400' : ''} border-r-2 border-dashed flex items-center`} onClick={() => setSelectProject(selectId(1))}>
+                  <div className={`cursor-pointer ${selectProject?.pjId === 1 ? 'bg-neutral-400' : ''} border-r-2 border-dashed flex items-center`} onClick={async() => setSelectProject(await selectId(1))}>
                     <span >특허 빅데이터 분석 서비스 데이터 구축 사업</span>
                   </div>
-                  <div className={`cursor-pointer ${selectProject?.pj_id === 2 ? 'bg-neutral-400' : ''} flex items-center`} onClick={() => setSelectProject(selectId(2))}>
+                  <div className={`cursor-pointer ${selectProject?.pjId === 2 ? 'bg-neutral-400' : ''} flex items-center`} onClick={async() => setSelectProject( await selectId(2))}>
                     <span>건국대학교 색인·검색 시스템 구축 사업</span>
                   </div>
                 </div>
@@ -114,45 +118,45 @@ export default function EmploymentHistory(){
               <div className='flex flex-col border-t-4 border-b-4 border-black w-full'>
                 <h2 className='border-b-4 h-10 border-black  flex justify-center items-center'>유지보수 경력</h2>
                 <div className='flex-row *:h-32 *:text-md flex *:w-1/2'>
-                  <div className={`cursor-pointer ${selectProject?.pj_id === 3 ? 'bg-neutral-400' : ''} border-r-2 border-dashed flex items-center`} onClick={() => setSelectProject(selectId(3))}>
+                  <div className={`cursor-pointer ${selectProject?.pjId === 3 ? 'bg-neutral-400' : ''} border-r-2 border-dashed flex items-center`} onClick={async() => setSelectProject(await selectId(3))}>
                     <span>국민연금공단 콜 분석 시스템 유지보수</span>
                   </div>
-                  <div className={`cursor-pointer ${selectProject?.pj_id === 4 ? 'bg-neutral-400' : ''} border-r-2 border-dashed flex items-center`} onClick={() => setSelectProject(selectId(4))}>
+                  <div className={`cursor-pointer ${selectProject?.pjId === 4 ? 'bg-neutral-400' : ''} border-r-2 border-dashed flex items-center`} onClick={async() => setSelectProject(await selectId(4))}>
                     <span>한국무역협회 TradePro, TradeNavy 색인 유지보수</span>
                   </div>
-                  <div className={`cursor-pointer ${selectProject?.pj_id === 5 ? 'bg-neutral-400' : ''} flex items-center`} onClick={() => setSelectProject(selectId(5))}>
+                  <div className={`cursor-pointer ${selectProject?.pjId === 5 ? 'bg-neutral-400' : ''} flex items-center`} onClick={async() => setSelectProject(await selectId(5))}>
                     <span>부산대병원/양산부산대병원/부산대한방병원 사이트 유지보수</span>
                   </div>
                 </div>
               </div>
-            </div>
+            </div>``
             {selectProject && (
               <div className="w-full flex flex-col p-4">
                 <div className="border-b-2 border-black pb-4">
-                  <h1 className="text-center text-3xl font-bold">{selectProject.pj_title}</h1>
+                  <h1 className="text-center text-3xl font-bold">{selectProject.pjTitle}</h1>
                 </div>
                 <div className='flex flex-col pt-2 gap-2'>
-                  <p><span className='font-bold'>회사/기관명 </span>: {selectProject.pj_company}</p>
-                  <p><span className='font-bold'>프로젝트 기간 </span> : {selectProject.pj_period}</p>
-                  <p><span className='font-bold'>참여인원 </span> : {selectProject.pj_man}</p>
-                  {selectProject.pj_participation && (<p><span className='font-bold'>참여도 </span>: {selectProject.pj_participation}</p>)}
+                  <p><span className='font-bold'>회사/기관명 </span>: {selectProject.pjCompany}</p>
+                  <p><span className='font-bold'>프로젝트 기간 </span> : {selectProject.pjPeriod}</p>
+                  <p><span className='font-bold'>참여인원 </span> : {selectProject.pjMan}</p>
+                  {selectProject.pjParticipation && (<p><span className='font-bold'>참여도 </span>: {selectProject.pjParticipation}</p>)}
                   
-                  <p><span className='font-bold'>사용 기술 </span>: {selectProject.pj_skill.map(((sk,index) => <span key={index}>{sk}{index !== selectProject.pj_skill.length - 1 ? ', ' : ''}</span>))}</p>
+                  <p><span className='font-bold'>사용 기술 </span>: {selectProject.skills.map(((sk,index) => <span key={index}>{sk}{index !== selectProject.skills.length - 1 ? ', ' : ''}</span>))}</p>
                   <p><span className='font-bold'>주요 업무</span></p>
                   <div className='*:pl-3'>
-                    {selectProject.pj_work.map((work,index) =><li key={index}>{work}</li>)}
+                    {selectProject.works.map((work,index) =><li key={index}>{work}</li>)}
                   </div>
-                  {selectProject.pj_result &&(<>
+                  {selectProject.results &&(<>
                     <p><span className='font-bold'>성과 및 경험</span></p>
                     <div className='*:pl-3'>
-                      {selectProject.pj_result.map((result, index) => <li key={index}>{result}</li>)}
+                      {selectProject.results.map((result, index) => <li key={index}>{result}</li>)}
                     </div>
                   </>
                   )}
-                  {selectProject.pj_review && (<>
+                  {selectProject.reviews && (<>
                     <p><span className='font-bold text-2xl'>Review</span></p>
                     <div className='*:pl-3'>
-                      {selectProject.pj_review.map((review, index) => <li key={index}>{review}</li>)}
+                      {selectProject.reviews.map((review, index) => <li key={index}>{review}</li>)}
                     </div>
                   </>
                   )}
